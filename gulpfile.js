8041991f2c35ef21c2524cec7c61ed055b198029
Sergey Loysha <sergeyloysha@gmail.com>
2016-10-20 21:33:31 +0300

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserSync   = require('browser-sync');
 var sass          = require('gulp-sass');
 var postcss       = require('gulp-postcss');
 var autoprefixer  = require('autoprefixer');
+var ghPages       = require('gulp-gh-pages');
 var reload        = browserSync.reload;
 
 gulp.task('sass', function () {
@@ -16,6 +17,11 @@ gulp.task('sass', function () {
     .pipe(postcss(processors))
     .pipe(gulp.dest('./css'))
     .pipe(reload({stream: true}));
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./**/*')
+    .pipe(ghPages());
 });
 
 gulp.task('default', ['sass'],function () {
